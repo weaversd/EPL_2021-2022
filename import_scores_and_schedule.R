bf5b593866100fc1106c9cf9073638de28b18775
weaversd <sweaver4@nd.edu>
2022-01-10 16:28:46 -0500

    changed import methodo to detect type of string

--- import_scores_and_schedule.R
+++ import_scores_and_schedule.R
@@ -2,13 +2,50 @@
 schedule <- read_html("https://fbref.com/en/comps/9/schedule/Premier-League-schedule-and-Fixtures")
 #schedule_html <- html_nodes(schedule, 'th.right , .left:nth-child(3) a, .left:nth-child(9) a, .right a')
 #schedule_html <- html_nodes(schedule, '.left:nth-child(3) a , .left:nth-child(7) a, .right a, th.right')
-schedule_html <- html_nodes(schedule, 'th.right , .left:nth-child(9) a, .right a, .left:nth-child(3) a')
+#schedule_html <- html_nodes(schedule, 'th.right , .left:nth-child(9) a, .right a, .left:nth-child(3) a')
+schedule_html <- html_nodes(schedule, 'th.right , .left:nth-child(9) a, .right a, .left:nth-child(3) a, td.center')
 schedule_text <- html_text(schedule_html)
-schedule_text <- stri_remove_empty(schedule_text)
+#schedule_text <- stri_remove_empty(schedule_text)
 
+determine_if_date <- function(string) {
+  if (grepl("^[[:digit:]]{4}-[[:digit:]]{2}-[[:digit:]]{2}$", string)) {
+    return(T)
+  } else {
+    return(F)
+  }
+}
+
+date_list <- c()
+matchday_list <- c()
+home_list <- c()
+away_list <- c()
+score_list <- c()
+for (i in 1:length(schedule_text)) {
+  if (determine_if_date(schedule_text[i])) {
+    date <- schedule_text[i]
+    matchday <- schedule_text[i-1]
+    home <- schedule_text[i+1]
+    away <- schedule_text[i+3]
+    score <- schedule_text[i+2]
+    score <- str_replace_all(score, pattern = "[^0-9]", "~")
+    date_list <- append(date_list, date)
+    matchday_list <- append(matchday_list, matchday)
+    home_list <- append(home_list, home)
+    away_list <- append(away_list, away)
+    score_list <- append(score_list, score)
+  }
+}
+
+score_df <- data.frame(Matchday = matchday_list,
+                       Date = date_list,
+                       Home = home_list,
+                       Away = away_list,
+                       rawscore = score_list)
+
+score_df$Date <- as.Date(score_df$Date)
 
 #set the number of imported columns
-imported_columns_n <- 4
+#imported_columns_n <- 5
 
 # #create a dataframe to hold the schedule
 # schedule_df <- data.frame(matrix(ncol = imported_columns_n))
@@ -27,16 +64,16 @@ imported_columns_n <- 4
 # }
 
 
-schedule_matrix <- matrix(schedule_text, ncol = imported_columns_n, byrow = T)
-schedule_df <- as.data.frame(schedule_matrix, stringsAsFactors = F)
+#schedule_matrix <- matrix(schedule_text, ncol = imported_columns_n, byrow = T)
+#schedule_df <- as.data.frame(schedule_matrix, stringsAsFactors = F)
 
 
 #rename columns
-colnames(schedule_df) <- c("Matchday", "Date", "Home", "Away")
-schedule_df$Date <- as.Date(schedule_df$Date)
+#colnames(schedule_df) <- c("Matchday", "Date", "Home", "Away")
+#schedule_df$Date <- as.Date(schedule_df$Date)
 
 #create_scores_df
-scores_df <- schedule_df
+scores_df <- score_df
 
 #add columns to score df
 scores_df$H_score <- NA
@@ -48,21 +85,21 @@ scores_df$Draw <- NA
 
 
 #get scores
-score_import <- read_html("https://fbref.com/en/comps/9/schedule/Premier-League-schedule-and-Fixtures")
-score_import_html <- html_nodes(score_import, 'td.center')
-score_import_text <- html_text(score_import_html)
+#score_import <- read_html("https://fbref.com/en/comps/9/schedule/Premier-League-schedule-and-Fixtures")
+#score_import_html <- html_nodes(score_import, 'td.center')
+#score_import_text <- html_text(score_import_html)
 #score_import_text <- score_import_text[-(seq(11,417,11))]
-score_import_text <- score_import_text[-c(11,22,33,44,55,66,77,88,99,110,
-                                          121,132,143,154,165,176, 187,195,206,
-                                          217,228,232,243,254,265,276,287,
-                                          298,309,320,331,342,353,364,375,386,
-                                          397,408)]
-is.na(score_import_text) <- score_import_text == ""
+#score_import_text <- score_import_text[-c(11,22,33,44,55,66,77,88,99,110,
+#                                          121,132,143,154,165,176, 187,195,206,
+#                                          217,228,232,243,254,265,276,287,
+#                                          298,309,320,331,342,353,364,375,386,
+#                                          397,408)]
+#is.na(score_import_text) <- score_import_text == ""
 
-score_import_text <- str_replace_all(score_import_text, pattern = "[^0-9]", "~")
+#score_import_text <- str_replace_all(score_import_text, pattern = "[^0-9]", "~")
 
 #save scores in column
-scores_df$rawscore <- score_import_text
+#scores_df$rawscore <- score_import_text
 
 
 #parse the text to get scores
--- weekly_analysis.R
+++ weekly_analysis.R
@@ -171,7 +171,7 @@ points_plot <- ggplot(data = weekly_table, aes(x = matchday, y = Pts)) +
         axis.text = element_text(size = 20),
         axis.title = element_text(size = 20)) +
   scale_x_continuous(breaks = function(x) seq(ceiling(x[1]), floor(x[2]), by = 1)) +
-  scale_y_continuous(breaks = function(x) seq(ceiling(x[1])+1, floor(x[2]), by = 2)) +
+  scale_y_continuous(breaks = function(x) seq(ceiling(x[1])+2, floor(x[2]), by = 2)) +
   labs(x = "Week", y = "Points", title = "2021-2022 EPL points by week") +
   geom_point(aes(fill = color), size = 3, shape = 21, stroke = 0.5) +
   scale_fill_identity()
@@ -193,7 +193,7 @@ GD_plot <- ggplot(data = weekly_table, aes(x = matchday, y = GD)) +
         axis.text = element_text(size = 20),
         axis.title = element_text(size = 20)) +
   scale_x_continuous(breaks = function(x) seq(ceiling(x[1]), floor(x[2]), by = 1)) +
-  scale_y_continuous(breaks = function(x) seq(ceiling(x[1]), floor(x[2]), by = 2)) +
+  scale_y_continuous(breaks = function(x) seq(ceiling(x[1]), floor(x[2]), by = 3)) +
   labs(x = "Week", y = "Goal Differential", title = "2021-2022 EPL goal differential by week") +
   geom_point(aes(fill = color), size = 3, shape = 21, stroke = 0.5) +
   scale_fill_identity()
@@ -237,7 +237,7 @@ GA_plot <- ggplot(data = weekly_table, aes(x = matchday, y = GA)) +
         axis.text = element_text(size = 20),
         axis.title = element_text(size = 20)) +
   scale_x_continuous(breaks = function(x) seq(ceiling(x[1]), floor(x[2]), by = 1)) +
-  scale_y_continuous(breaks = function(x) seq(ceiling(x[1])+1, floor(x[2]), by = 1)) +
+  scale_y_continuous(breaks = function(x) seq(ceiling(x[1])+2, floor(x[2]), by = 2)) +
   labs(x = "Week", y = "Goals against", title = "2021-2022 EPL goals against by week") +
   geom_point(aes(fill = color), size = 3, shape = 21, stroke = 0.5) +
   scale_fill_identity()
